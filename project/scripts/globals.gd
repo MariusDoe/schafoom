f3450fe196a672ce290d4c0da99c560f94dad7ed
Matvey <Matvey.Sivashinskiy@student.hpi.uni-potsdam.de>
2021-03-28 11:30:46 +0200

    Added Music

--- project/scripts/globals.gd
+++ project/scripts/globals.gd
@@ -5,11 +5,24 @@ var time: float
 var potato_count: int
 var in_game = false
 
+var music_player: AudioStreamPlayer
+
+func _ready():
+	music_player = AudioStreamPlayer.new()
+	add_child(music_player)
+	main_menu()
+
 func _physics_process(delta):
 	if in_game:
 		time += delta
 
 func start_cutscene():
+	#var stream = preload("res://sfx/Music/Juhani Junkala [Chiptune Adventures] 1. Stage 1.wav")
+	#stream.loop_mode = AudioStreamSample.LOOP_FORWARD
+	#stream.loop_end = stream.get_length() * stream.mix_rate
+	#var volume = -15
+	#set_music(stream, volume)
+	music_player.stop()
 	get_tree().change_scene("res://scenes/Cutscene.tscn")
 
 func start_game():
@@ -17,6 +30,11 @@ func start_game():
 	time = 0.0
 	potato_count = 0
 	in_game = true
+	var stream = preload("res://sfx/Music/Juhani Junkala [Chiptune Adventures] 1. Stage 1.wav")
+	stream.loop_mode = AudioStreamSample.LOOP_FORWARD
+	stream.loop_end = stream.get_length() * stream.mix_rate
+	var volume = -15
+	set_music(stream, volume)
 	get_tree().change_scene("res://scenes/game.tscn")
 
 func dash(new_level: int):
@@ -25,9 +43,17 @@ func dash(new_level: int):
 
 func die():
 	in_game = false
+	var stream = preload("res://sfx/Player/death_jack_01.wav")
+	var volume = -5
+	set_music(stream, volume)
 	get_tree().change_scene("res://scenes/Game_Over.tscn")
 
 func main_menu():
+	var stream = preload("res://sfx/Music/Juhani Junkala [Chiptune Adventures] 2. Stage2.wav")
+	stream.loop_mode = AudioStreamSample.LOOP_FORWARD
+	stream.loop_end = stream.get_length() * stream.mix_rate
+	var volume = -15
+	set_music(stream, volume)
 	get_tree().change_scene("res://scenes/Main_Menu.gd")
 
 func quit():
@@ -39,3 +65,9 @@ func submit_score(name: String):
 
 func is_level_ok(level: int) -> bool:
 	return level >= 0 and level <= 2
+
+func set_music(stream: AudioStream, volume: float) -> void:
+	music_player.stop()
+	music_player.stream = stream
+	music_player.volume_db = volume
+	music_player.play()
